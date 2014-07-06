15fe4562db0ba8efd18001c2f973561f34a6a8f8
panda <pandabo1985@gmail.com>
2014-07-06 21:34:55 +0800

    Timer create add RunLoop

---
+++ NSTimer_RunLoop_0706/NSTimer_RunLoop_0706/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NSTimer_RunLoop_0706
+//
+//  Created by pan dabo on 14-7-6.
+//  Copyright (c) 2014年 pan dabo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ NSTimer_RunLoop_0706/NSTimer_RunLoop_0706Tests/NSTimer_RunLoop_0706Tests.m
@@ -0,0 +1,34 @@
+//
+//  NSTimer_RunLoop_0706Tests.m
+//  NSTimer_RunLoop_0706Tests
+//
+//  Created by pan dabo on 14-7-6.
+//  Copyright (c) 2014年 pan dabo. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface NSTimer_RunLoop_0706Tests : XCTestCase
+
+@end
+
+@implementation NSTimer_RunLoop_0706Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
